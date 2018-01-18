05b346b7f2e2f1ccadc5195ddcbda774b3d22f79
Arvind Surana <arvind.surana@live.com>
2018-01-18 16:44:31 +0530

    * .gitignore: Initial Setup
    * MyGeoFenceiOS.sln:
    * Main.cs:
    * Info.plist:
    * MyClass.cs:
    * AppDelegate.cs:
    * Main.storyboard:
    * ViewController.cs:
    * Entitlements.plist:
    * MyGeoFenceiOS.csproj:
    * LaunchScreen.storyboard:
    * MyGeoFence.Core.csproj:
    * ViewController.designer.cs:
    * AssemblyInfo.cs:
    * Contents.json:
    * Contents.json:

---
+++ MyGeoFence.Core/MyClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace MyGeoFence.Core
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+
+            Debug.WriteLine("Success");
+
+        }
+    }
+}
